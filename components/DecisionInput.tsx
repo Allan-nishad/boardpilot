f7a766cd7a3cd9a5b56dd4768ad838dcbf521763
allan-nishad <allannishad@gmail.com>
2026-07-18 16:37:30 +0530

    feat: support NEXT_PUBLIC_API_URL environment variable for production deployments

--- components/DecisionInput.tsx
+++ components/DecisionInput.tsx
@@ -214,7 +214,8 @@ export default function DecisionInput() {
     }
 
     try {
-      const response = await fetch("http://localhost:8000/api/boardroom", {
+      const apiBase = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+      const response = await fetch(`${apiBase}/api/boardroom`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
